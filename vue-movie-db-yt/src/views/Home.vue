e444811de1f9baadb9c262478815e9d8a81cf059
RedaMeziouni <meziounidb@gmail.com>
2021-06-27 17:51:55 +0100

    Added preview image (using OMDB API key) and description

--- vue-movie-db-yt/src/views/Home.vue
+++ vue-movie-db-yt/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+    
+<!--tt0409591 (OMDb API Key) give us a movie trailer from omdb-->
+      <router-link to="/movie/tt0409591">
+        <img src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg" alt="Naruto Poster" class="featured-img" />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
